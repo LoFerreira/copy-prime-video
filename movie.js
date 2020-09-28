1b118085502c4e7010149b464b8a489290a54202
Leonardo Ferreira <leonardo10sp@gmail.com>
2020-09-28 18:57:59 -0300

    archives update js and css

--- movie.js
+++ movie.js
@@ -1,9 +1,18 @@
-var main = document.querySelectorAll(".main .center")[0];
+var main = document.querySelectorAll('.main .center')[0];
 
-fetch("https://api.themoviedb.org/3/movie/550?api_key=7c5bc6b618b4dec99477c43314413c2d"){
-    .then(response=> response.json())
-    .then(data=>{
-        console.log(data);
+fetch("https://api.themoviedb.org/3/discover/movie?api_key=7c5bc6b618b4dec99477c43314413c2d&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1")
+.then(response=> response.json())
+.then(data=>{
+    data.results.map((i,j)=>{
+        if(j > 10)
+            return;
+        if(j > 0){
+            main.innerHTML+='<div class="banner-principal" style="background-image:url=https://image.tmdb.org/t/p/'+i.popularity+'"></div>';
+        }else{
+            if(j == 1){
+
+            }
+        }
     })
+})
     
-}
